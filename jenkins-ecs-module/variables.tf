c82b84fde734494694e7ca1dee9170c87ed00430
kbikas <kostasbikas02@gmail.com>
2026-09-13 01:10:54 +0300

    added module level variables

---
+++ jenkins-ecs-module/variables.tf
@@ -0,0 +1,61 @@
+variable "cloudwatch_name" {
+  description = "The name of the CloudWatch group to make"
+  type        = string
+  default     = "jenkins-logs"
+}
+
+variable "task_role_arn" {
+  type    = string
+}
+
+variable "agent_tunnel_port" {
+  description = "Port that the fargate agent will reach to communicate with the jenkins controller"
+  type        = number
+  default     = 5000
+}
+
+variable "ecs_clusters" {
+  type    = list(string)
+  default = ["jenkins_ecs_cluster"]
+}
+
+variable "task_definitions" {
+  type = list(object({
+    name_tag    = string
+    family_name = string
+    container = object({
+      image   = string
+      cw_name = string
+    }),
+    cpu    = string
+    memory = string
+  }))
+}
+
+variable "vpc_id" {
+  type    = string
+  default = "vpc-0a0c16129cd4e9a26"
+}
+
+variable "my_public_ip" {
+  type = string
+}
+
+variable "jenkins_ssh_key_name" {
+  type    = string
+  default = "jenkins-key"
+}
+
+variable "jenkins_ssh_key_path" {
+  type = string
+}
+
+variable "jenkins_instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "jenkins_subnet" {
+  type = string
+  default = "subnet-020b488a8d6851690"
+}
